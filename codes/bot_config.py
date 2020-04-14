4d0b81b501b4311a8be1b443439dbf7d932468a3
LEE SEUNGEUN <seungeun.data@gmail,com>
2020-04-14 05:11:45 +0000

    add complementory materials to chap04-1

--- codes/bot_config.py
+++ codes/bot_config.py
@@ -1,5 +1,5 @@
 SLACK_BOT_CONFIG = {
-    'api-token':'xoxb-1024668750400-1013225911907-lEZHoCZLc1BeuLRCmh6a8F1G',
+    'api-token':'xoxb-1024668750400-1013225911907-LEUpWezAlC8AxU2dcLzkavl6',
     'bot_user_id': 'dobby',
     'db': '/projects/fluent-python/codes/bot.db'
 }
